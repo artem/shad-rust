a389088a23890cb10d2be570dcdadc6f0f049cc0
Sergei Fomin <gh4exh3w62@fastmail.com>
2021-10-16 18:43:51 +0300

    Add unimplemented!() do gc-derive

--- gc/gc-derive/src/lib.rs
+++ gc/gc-derive/src/lib.rs
@@ -5,5 +5,5 @@ use syn::{parse_macro_input, Data, DeriveInput, FieldsNamed, Ident, Type};
 #[proc_macro_derive(Scan)]
 pub fn derive_scan(input: TokenStream) -> TokenStream {
     // TODO: your code here.
+    unimplemented!()
 }
-
